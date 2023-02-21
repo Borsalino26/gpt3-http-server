5217e5560e9864d69f3f692615d8c034c0e35f0d
Borsalino <2595440974@qq.com>
2023-02-21 23:36:22 +0800

    change default port

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ package main
 import "github.com/cloudwego/hertz/pkg/app/server"
 
 func main() {
-	h := server.Default()
+	h := server.Default(
+		server.WithHostPorts("127.0.0.1:6789"),
+	)
 	register(h)
 	h.Spin()
 }
