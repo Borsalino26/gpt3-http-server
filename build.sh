289be98a7ab774f24981e4560bff3fd1db3f103f
Borsalino <2595440974@qq.com>
2023-02-25 19:43:54 +0800

    add run comment

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-# !/bin/bash
-if [ !-d "$output"]
-then
-  mkdir output
-fi
+#!/bin/bash
 
+mkdir -p output
 go build && mv gpt3-http-server ./output/gpt3-http-server && mv config.json ./output/config.json
+echo "./gpt3-http-server > out.log" > run.sh
