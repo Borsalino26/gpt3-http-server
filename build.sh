0e1c2c6fab3c64ebc8cecba0cfda84d2d055c36b
Borsalino <2595440974@qq.com>
2023-02-25 20:36:08 +0800

    build command

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p output
-go build && mv gpt3-http-server ./output/gpt3-http-server && mv config.json ./output/config.json
+go build && mv gpt3-http-server ./output/gpt3-http-server && cp config.json ./output/config.json && mv run.sh ./output/run.sh
 echo "./gpt3-http-server > out.log 2>&1" > run.sh
